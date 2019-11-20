3b66b7f2e04c5ea79f4f32c3529970b5cbd0d38e
Roger Silva <roger.silva@picpay.com>
2019-11-20 02:04:54 -0300

    Implementação de teste da biblioteca
    - Simples exemplo de implementação da biblioteca de servicelocator
    
    Signed-off-by: Roger Silva <roger.silva@picpay.com>

--- app/src/main/java/com/example/librarysample/MainActivity.kt
+++ app/src/main/java/com/example/librarysample/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.example.librarysample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.servicelocator.ServiceLocator.getServiceInstance
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val user = getServiceInstance<User>()
+
+        message.text = user.name
+
     }
 }
